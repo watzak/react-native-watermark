bf943b63dcfacb822c76c841963e74c5e50ee48c
watzak <kzahirag@gmail.com>
2015-11-16 10:27:52 +0100

    Initial Commit

---
+++ RNWatermark/RNWatermark.h
@@ -0,0 +1,13 @@
+//
+//  RNWatermark.h
+//  RNWatermark
+//
+//  Created by watzak on 16/11/15.
+//  Copyright © 2015 watzak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNWatermark : NSObject
+
+@end
